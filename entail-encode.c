bcd00c96ce899e17a18ce0e0dad129cb1b22d76f
Marijn Heule <mheule@Marijns-Air.lan>
2023-11-10 10:06:45 -0500

    clean up

--- entail-encode.c
+++ entail-encode.c
@@ -10,38 +10,32 @@ int main (int argc, char** argv) {
   left  = fopen (argv[1], "r");
   right = fopen (argv[2], "r");
 
-//  while (1) {
-    tmp = fscanf (left, " p cnf %i %i ", &lVar, &lCls);
-    if (tmp != 2) {
-      printf ("c ERROR: no header file detected in %s\n", argv[1]);
-      exit (1); }
-//  }
+  tmp = fscanf (left, " p cnf %i %i ", &lVar, &lCls);
+  if (tmp != 2) {
+    printf ("c ERROR: no header file detected in %s\n", argv[1]);
+    exit (1); }
 
-//  while (1) {
+  rLit = 0;
+  tmp = fscanf (right, " p cnf %i %i ", &rVar, &rCls);
+  if (tmp != 2) {
+    printf ("c ERROR: no header file detected in %s\n", argv[2]);
+    exit (1); }
 
-    rLit = 0;
-    tmp = fscanf (right, " p cnf %i %i ", &rVar, &rCls);
-    if (tmp != 2) {
-      printf ("c ERROR: no header file detected in %s\n", argv[2]);
-      exit (1); }
+  while (1) {
+    tmp = fscanf (right, " %i ", &lit);
+    if (tmp == EOF) break;
+    if (lit != 0) rLit++; }
+  fclose (right);
 
-    while (1) {
-      tmp = fscanf (right, " %i ", &lit);
-      if (tmp == EOF) break;
-      if (lit != 0) rLit++;
-    }
-    fclose (right);
-
-    nVar = rVar + rCls;
-    nCls = lCls + rLit + 1;
-    printf ("p cnf %i %i\n", nVar, nCls);
+  nVar = rVar + rCls;
+  nCls = lCls + rLit + 1;
+  printf ("p cnf %i %i\n", nVar, nCls);
 
   while (1) {
     tmp = fscanf (left, " %i ", &lit);
     if (tmp == EOF) break;
     if (lit != 0) printf ("%i ", lit);
-    else          printf ("0\n");
-  }
+    else          printf ("0\n"); }
   fclose (left);
 
   for (int i = 1; i <= rCls; i++)
@@ -60,11 +54,7 @@ int main (int argc, char** argv) {
     tmp = fscanf (right, " %i ", &lit);
     if (tmp == EOF) break;
     if (lit != 0) printf ("%i %i 0\n", -aux, -lit);
-    if (lit == 0) aux++;
-  }
+    if (lit == 0) aux++; }
   fclose (right);
-
-
-//  }
 
 }
